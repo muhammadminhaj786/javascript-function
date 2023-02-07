7c186f96fa40130dcc24e21dcec07dff87cc756e
muhammadminhaj786 <122244089+muhammadminhaj786@users.noreply.github.com>
2023-02-07 04:30:47 -0800

    work completed

--- app.js
+++ app.js
@@ -1,64 +1,59 @@
 // // Varaiables
-// var firstName = "minhaj";
-// var lastName ="wahid";
-// var fullName = firstName + " " + lastName;
-// console.log("Hello! This is" + " " +fullName);
-// var a = 10
-// var b ="20"
-// var c = a+b
-// console.log(c)
+var firstName = "minhaj";
+var lastName ="wahid";
+var fullName = firstName + " " + lastName;
+console.log("Hello! This is" + " " +fullName);
+var a = 10
+var b ="20"
+var c = a+b
+console.log(c)
 
 // //Functions
 
-// function prod(num1,num2){
-//     var num1 = 5
-//     var num2 = 10
-//     var mul =num1*num2;
-//     alert(mul)
-// }
-// prod();
-// console.log(prod(num1,num2))
+function prod(num1,num2){
+    var num1 = 5
+    var num2 = 10
+    var mul =num1*num2;
+    alert(mul)
+}
+prod();
+console.log(prod(num1,num2))
 
-// document.getElementById("demo").innerHTML = fullName;
+document.getElementById("demo").innerHTML = fullName;
 
 // //Write a function code
-// var num3=prompt("Enter first number")
-// var num4=prompt("Enter second number")
-// function myFunction(num3,num4){
-//     return num3*num4;
-// }
-// document.getElementById("demo1").innerHTML = myFunction(num3,num4)
+var num3=prompt("Enter first number")
+var num4=prompt("Enter second number")
+function myFunction(num3,num4){
+    return num3*num4;
+}
+document.getElementById("demo1").innerHTML = myFunction(num3,num4)
 
 //convert frahenist to celcius
-// prompt("Enter Fraheinst")
-// function tocelsius(frahenist){
-//     return (5/9) * (frahenist-32);
-// }
-// document.getElementById("demo3").innerHTML = tocelsius;
+frahenist=prompt("Enter Fraheinst")
+function tocelsius(frahenist){
+    return (5/9) * (frahenist-32);
+}
+document.getElementById("demo3").innerHTML = tocelsius;
 
 // create a function find number is even or odd
-// function even_odd(num5){
-//     var num5 = prompt("Enter a number")
-//     if (num5%2==0){
-//         console.log("This is even number")
-//     }
-//     else {
-//         console.log("This is odd number")
-//     }    
-// }
-// even_odd()
+function even_odd(num5){
+    var num5 = prompt("Enter a number")
+    if (num5%2==0){
+        console.log("This is even number")
+    }
+    else {
+        console.log("This is odd number")
+    }    
+}
+even_odd()
 
 //return te square of number
 
-// function Square(num6){
-//     var num6= prompt("Enter a number")
-//     var sq_num= num6**2
-//     console.log(sq_num)
-// }
-// Square()
-
-//find vowels
-funtion Vowels(inp){
-    inp=prompt("Enter a setence")
-    if (inp)
+function Square(num6){
+    var num6= prompt("Enter a number")
+    var sq_num= num6**2
+    console.log(sq_num)
 }
+Square()
+
